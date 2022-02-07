bc51ec40a920619c895bb3d3d74d6b8949e07618
b-kip <bruzkip@gmail.com>
2022-02-07 18:30:44 +0300

    Reexports

--- ES6/main.js
+++ ES6/main.js
@@ -1,11 +1,9 @@
-// import { counter, incrementCounter } from './lib.js';
 import * as lib from './lib.js'; // namespace import
+import * as reexport from './reexport.js';
 import logger from './logger.js';
 
-// console.log(lib);
+console.log(reexport);
 
-// allows be 4. Exported an expression rather than variable
-// that can be pointed at.
 console.log('Count', lib.default);
 
 console.log(lib.counter); // counter === 4 because logger has been executed.
@@ -13,15 +11,8 @@ lib.incrementCounter();
 console.log(lib.counter); // counter === 5.
 loadLogger(); // counter = 5
 
-// allows be 4. Exported an expression rather than variable
-// that can be pointed at.
-// console.log('Count', count);
 
 function loadLogger() {
   // import logger from './logger.js'; // should be at top level
   logger()
 }
-
-// imports are hoisted.
-// import count from './lib.js';
-// count = 3; // Error: assignment to a constant variable.
